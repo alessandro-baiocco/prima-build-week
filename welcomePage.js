3dbd511ff65ce9495f042e1db6261621d47da5a7
Simo095 <simone.dangelo636@gmail.com>
2023-07-19 15:03:23 +0200

    Funzia

---
+++ welcomePage.js
@@ -0,0 +1,12 @@
+const scatolaCheck = document.getElementsByTagName("input")[0];
+const bottone = document.getElementsByTagName("button")[0];
+
+scatolaCheck.onchange = function () {
+  if (scatolaCheck.checked) {
+    bottone.disabled = false;
+  } else {
+    bottone.disabled = true;
+  }
+};
+
+const noRefresh = (vaiAvanti) => vaiAvanti.preventDefault();
