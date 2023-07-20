4f958b48b09b889ef42aa8d78f02dce4a2772494
Simo095 <simone.dangelo636@gmail.com>
2023-07-20 12:20:36 +0200

    aggiunto sort

--- benchmark copy.js
+++ benchmark copy.js
@@ -205,14 +205,15 @@ let domandaRandom = () => {
     h1.innerHTML = questions[rand].question;
     const risposte = questions[rand].incorrect_answers;
     risposte.push(questions[rand].correct_answer);
+    risposte.sort();
     const bottoneUno = document.getElementById("primo");
     bottoneUno.innerHTML = risposte[3];
     const bottoneDue = document.getElementById("secondo");
-    bottoneDue.innerHTML = risposte[0];
+    bottoneDue.innerHTML = risposte[1];
     const bottoneTre = document.getElementById("terzo");
     bottoneTre.innerHTML = risposte[2];
     const bottoneQuarto = document.getElementById("quarto");
-    bottoneQuarto.innerHTML = risposte[1];
+    bottoneQuarto.innerHTML = risposte[0];
     //--------------------------------rimozione bottoni per due risposte------------------------
     if (bottoni1.innerText === "undefined") {
       //disablita i bottoni se ci sono 2 risposte
